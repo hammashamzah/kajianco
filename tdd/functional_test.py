e20b23bef183f2d4fe165e3c445ee3ecae26fabc
Hammas Hamzah Kuddah <hammas.hamzah@gmail.com>
2018-01-16 23:01:25 +0100

    Functional test now checks we can input a to-do item

--- tdd/functional_test.py
+++ tdd/functional_test.py
@@ -2,10 +2,14 @@
 # It follows a user story
 import unittest
 import argparse
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 HEADLESS = True
-VERBOSE = True
+VERBOSE = False
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -26,21 +30,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
 
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+        
         # He types "Buy shawarma" into a text box
+        inputbox.send_keys('Buy shawarma')
 
         # When He hits enter, the page updates, and now the page lists
         # "1: Buy shawarma" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy shawarma' for row in rows)
+        )
         # There is still a text box inviting his to add another item. He
         # enters "Eat shawarma"
-
+        self.fail('Finish the test')
         # The page updates again, and now shows both items on his list
 
-        # Edith wonders whether the site will remember his list. Then He sees
+        # Hammas wonders whether the site will remember his list. Then He sees
         # that the site has generated a unique URL for his -- there is some
         # explanatory text to that effect.
 
